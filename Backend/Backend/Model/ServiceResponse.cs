270bca6a2d9cfdac7ca7ca642bf94ede09215b8c
srikaaaaa1234 <milestonetags@gmail.com>
2024-05-24 16:13:52 +0530

    Upgraded the version to support .net 8

--- Backend/Backend/Model/ServiceResponse.cs
+++ Backend/Backend/Model/ServiceResponse.cs
@@ -2,7 +2,7 @@
 {
     public class ServiceResponse<T>
     {
-        public T Data { get; set; }
+        public T? Data { get; set; }
         public bool Status { get; set; } = true;
         public string Message { get; set; } = null;
     }
